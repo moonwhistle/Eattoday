b86bcd69edda831ac46d5bea5ab308af322c51e9
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-02-19 14:51:05 +0900

    init project

---
+++ src/main/java/com/eattoday/Eattoday/EattodayApplication.java
@@ -0,0 +1,13 @@
+package com.eattoday.Eattoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EattodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EattodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eattoday/Eattoday/EattodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eattoday.Eattoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EattodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
